81452c27215e50bc1fdfc8ce22b9abfe7bef1cc6
Rahmad firdiansyah <90381140+Rahmadfirdiansyah@users.noreply.github.com>
2024-01-15 21:39:26 +0700

    Create prak 2 no 1.cpp

---
+++ prak 2 no 1.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+
+using namespace std;
+
+int main(int argc, char *argv[])
+{
+	int matA[2][2] = {4, 2, 8, 10};
+	int matB[2][2] = {-5, 8, 4, -12};
+	int j, i;
+	cout <<"matriks A"<<endl;
+	for (int i=0; i<2; i++){
+		cout<<matA[i][j]<<" ";
+	}
+	cout<<endl;
+
+	
+	cout<<"matriks B"<<endl;
+	for (int i=0; i<2; i++){
+		cout<<matB[i][j]<<"";}
+		
+	
+	cout<<endl;
+	{
+		cout<<"hasil penjumlahan matriks A dan B"<<endl;
+		for (int i=0; i<2; i++){
+			cout<<matA[i][j]+matB[i][j]<<"";
+		}
+		cout<<endl;
+	}
+	
+	return 0;
+	
+	}
